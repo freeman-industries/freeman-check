c17703ac37c24092058522fe9ee31a0c00737bf5
Nabil Freeman <nabil@instafork.com>
2023-08-12 10:47:53 +0800

    check is fully TSed

---
+++ src/Check.ts
@@ -0,0 +1,81 @@
+import assert from 'assert';
+import { CheckError } from './CheckError';
+import { validate, Schema } from 'jsonschema';
+import anora from 'anora';
+
+export class Check {
+	private schema: Schema;
+
+	constructor(schema: Schema) {
+		assert(schema, new CheckError(`Schema must be defined in constructor.`));
+
+		this.schema = schema;
+	}
+
+	test(object: unknown) {
+		if (object === null || object === undefined) {
+			throw new CheckError('The first argument is null or undefined.');
+		}
+
+		assert(this.schema, new CheckError(`test can't be called on Check instances initialized without a schema.`));
+
+		// validate.
+		const result = validate(object, this.schema);
+
+		// if there are no errors, the object is valid.
+		if (result.errors.length === 0) {
+			return;
+		}
+
+		// init error string
+		let error_message = '';
+
+		result.errors.forEach(error => {
+			let subject = String(error.argument); // name, email, etc.
+			let problem = 'is incorrect'; // default
+
+			// assign plain English to error messages.
+			switch (error.name) {
+				case 'additionalProperties':
+					problem = 'is not allowed';
+					break;
+				case 'required':
+					problem = 'is missing';
+					break;
+				case 'format':
+					problem = 'is malformatted';
+					break;
+				case 'type':
+					subject = error.property.replace('instance.', '');
+
+					let type: string | undefined = undefined;
+
+					if (typeof error.schema === 'string') {
+						type = error.schema;
+					} else if (typeof error.schema.type !== 'string') {
+						type = JSON.stringify(error.schema.type);
+					} else {
+						type = error.schema.type;
+					}
+
+					assert(
+						type,
+						new CheckError(`Validation failed, and then there was an internal error while determining the required type of ${subject}.`)
+					);
+
+					problem = 'needs to be ' + anora(type) + ' `' + type + '`';
+					break;
+			}
+
+			// concatenate all validation errors.
+			error_message += '`' + subject + '` ' + problem + '. ';
+		});
+
+		// remove last character from concatenated error message.
+		error_message = error_message.slice(0, -1);
+
+		const error = new CheckError(error_message, this.schema);
+
+		throw error;
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Check } from './Check';
+import { CheckError } from './CheckError';
+
+export { Check };
+export { CheckError };
